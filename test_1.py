9269633ef1f7509211b0c604b5d2669f1671acd8
TheDavinator <70241745+TheDavinator@users.noreply.github.com>
2020-11-12 17:20:06 -0800

    Added pseudo code

--- test_1.py
+++ test_1.py
@@ -1,1 +1,39 @@
+""""
+Hundred_A_Day()
+
+Days = 10
+Pay = 100
+Bank_Account = 0
+
+While Days > 0:
+    Pay += Bank_Account
+    Days -= 1
+    
+Return Bank_Account
+
+Compound_A_Day()
+
+Days = 10
+Pay = 1
+Bank_Account = 0
+
+While Days > 0:
+    Pay += Banck_Account
+    Pay = Pay * 2
+
+Return Bank_Account
+
+Main ()
+
+Salary1 = Hundred_A_Day
+Salary2 = Compoud_A_Day
+
+If Salary1 > Salary2:
+    Print ("Option 1 is better")
+If Salary2 > Salary1:
+    Print("Option 2 is better")
+Else:
+    Print(“Option 1 and Option 2 pays the same”)
+"""
+
 
