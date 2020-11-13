ee4667b7002c3e00ae6e3d688beeb836533378d6
TheDavinator <70241745+TheDavinator@users.noreply.github.com>
2020-11-12 17:56:08 -0800

    exam-end added

---
+++ test_1.py
@@ -0,0 +1,32 @@
+
+def Hundred_A_Day(Days) :
+    Pay = 100
+    Bank_Account = 0
+    while Days > 0:
+        Pay += Bank_Account
+        Days -= 1
+        if Days == 0 :
+            break
+        return Bank_Account
+
+def Compound_A_Day(Days) :
+    Pay = 1
+    Bank_Account = 0
+    
+    while Days > 0 :
+        Pay += Bank_Account
+        Pay = Pay * 2
+        if Days == 0:
+            break
+        return Bank_Account
+
+def Main () :
+    Days = 10
+    Salary1 = Compound_A_Day(Days) 
+    Salary2 = Hundred_A_Day(Days)
+    if Salary1 < Salary2 :
+        print ("Option 1 is better")
+        if Salary2 < Salary1 :
+            print("Option 2 is better")
+            if Salary1 == Salary2 :
+                 print("Option 1 and Option 2 pays the same")
