4318b82cdd5aefbc3d0c47c962d80601a94d8877
TheDavinator <70241745+TheDavinator@users.noreply.github.com>
2020-11-12 17:10:13 -0800

    removed all

--- test_1.py
+++ test_1.py
@@ -1,14 +1,1 @@
-"""
-Hundred_A_Day:
 
-DAYS is 10
-PAY is 100
-Bank_Account = 0
-
-WHILE day is GREAT THAN 0 :
-    Pay is PlUS EQUAL TO Bank_Account
-    DAYS MINUS EQUAL 1
-        IF DAYS Equals 0:
-        break
-Return Bank_Account
-"""
