0b91905be2bdb013daac9e5c4dbdb5b8fb9a45f6
TheDavinator <70241745+TheDavinator@users.noreply.github.com>
2020-11-12 17:03:12 -0800

    added structed english to test_1.py

--- test_1.py
+++ test_1.py
@@ -1,14 +1,38 @@
 """
 Hundred_A_Day:
 
-DAYS is 10
-PAY is 100
-Bank_Account = 0
+Days is 10
+Pay is 100
+Bank_Account is 0
 
-WHILE day is GREAT THAN 0 :
+WHILE day is GREAT THAN 0 
     Pay is PlUS EQUAL TO Bank_Account
-    DAYS MINUS EQUAL 1
-        IF DAYS Equals 0:
-        break
+    Days MINUS EQUAL TO 1
+    IF DAYS Equals 0
+        BREAK
 Return Bank_Account
+
+Compound_A_Day
+
+Days is 10
+Pay is 1
+Bank_Account is 0
+
+WHILE days is GREAT THAN 0
+    Pay is PLUS EQUAL TO Bank_Account
+    Pay = pay TIMES 2
+    Days MINUS EQUAL TO 1
+    IF days EQUALS 0
+        BREAK
+Return Bank_Account
+
+Main
+
+Salary1 EQUALS Hundred_A_Day
+Salary2 EQAULS Compound_A_Day
+
+IF Salary1 is GREAT THAN Salary2
+    Print(“Option 1 is better”)
+ELSE:
+    Print(“Option 2 is better”)
 """
