f910e2e98d20fcc901300be783841c16198a9d05
TheDavinator <70241745+TheDavinator@users.noreply.github.com>
2020-12-06 16:30:19 -0800

    Changed a lot of stuff

--- test_1.py
+++ test_1.py
@@ -1,32 +1,30 @@
-
 def Hundred_A_Day(Days) :
     Pay = 100
-    Bank_Account = 0
+    Bank_Account1 = 0
     while Days > 0:
-        Pay += Bank_Account
+        Bank_Account1 += Pay
         Days -= 1
         if Days == 0 :
-            return Bank_Account
+            return Bank_Account1
 
 def Compound_A_Day(Days) :
     Pay = 1
-    Bank_Account = 0
-    
+    Bank_Account2 = 0
     while Days > 0 :
-        Pay += Bank_Account
-        Pay = Pay * 2
+        Bank_Account2 += Pay
+        Pay += Pay * 2
+        Days -= 1
         if Days == 0:
-            return Bank_Account
+            return Bank_Account2
 
 
 Days = 10
-Salary1 = Compound_A_Day(Days) 
-Salary2 = Hundred_A_Day(Days)
-if Salary1 > Salary2 :
-    print("Option 1 is better")
-    if Salary2 > Salary1 :
-        print("Option 2 is better")
-        if Salary1 == Salary2 :
-             print("Option 1 and Option 2 pays the same")
-
+Salary1 = Hundred_A_Day(Days) 
+Salary2 = Compound_A_Day(Days)
+if Salary2 < Salary1 :
+    print ("Option 1 is Better")
+    if Salary1 < Salary2 :
+        print("Option2 is better")
+        if (Salary1 == Salary2) :
+            print("They are the same!")
 
